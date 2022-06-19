b3d7e339d98e61ab1402db6a593c833c4845a86a
Pedro Machado <pedro.machado@getground.co.uk>
2022-06-19 21:49:15 +0100

    Add start game logic for invalid numbers and styling

--- App.tsx
+++ App.tsx
@@ -1,16 +1,27 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, ImageBackground } from "react-native";
 import StartGame from "./screens/startGame";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
-  return <StartGame />;
+  return (
+    <LinearGradient style={styles.container} colors={["#ddb52f", "#4e0329"]}>
+      <ImageBackground
+        source={require("./assets/images/background.png")}
+        resizeMode="cover"
+        style={styles.container}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGame />
+      </ImageBackground>
+    </LinearGradient>
+  );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.1,
+  },
+});
--- components/PrimaryButton/primaryButton.tsx
+++ components/PrimaryButton/primaryButton.tsx
@@ -3,19 +3,47 @@ import React, { ReactNode } from "react";
 
 interface Props {
   children: ReactNode;
-  onPress: () => void;
+  onPress?: () => void;
 }
 
 const PrimaryButton = ({ children, onPress }: Props) => {
   return (
-    <Pressable onPress={onPress}>
-      <View>
-        <Text>{children}</Text>
-      </View>
-    </Pressable>
+    <View style={styles.btnOutterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.btnInnerContainer, styles.pressed]
+            : styles.btnInnerContainer
+        }
+        onPress={onPress}
+        android_ripple={{ color: "#fff" }}
+      >
+        <Text style={styles.btnText}>{children}</Text>
+      </Pressable>
+    </View>
   );
 };
 
 export default PrimaryButton;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  btnInnerContainer: {
+    backgroundColor: "#72063c",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  btnText: {
+    color: "white",
+    textAlign: "center",
+    fontWeight: "600",
+  },
+  btnOutterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
