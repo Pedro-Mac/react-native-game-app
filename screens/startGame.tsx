f413a7692d7b5821cbcffb2beb49b60e5208ef13
Pedro Machado <pedro.machado@getground.co.uk>
2022-06-19 17:02:53 +0100

    Add screen

---
+++ screens/startGame.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View, TextInput } from 'react-native'
+import React from 'react'
+
+const StartGame = () => {
+  return (
+    <View>
+        <TextInput />
+    </View>
+  )
+}
+
+export default StartGame
+
+const styles = StyleSheet.create({})
